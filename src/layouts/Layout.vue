5da11664e6b776f9d3ee0c0e36df266ac0c785a3
saadayouj <saadayouj666@gmail.com>
2020-01-28 00:49:15 +0100

    Setup Quasar & done front end Todo & setup vuex

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title
+        class="absolute-center">
+          <b>Multi-Platform Todo</b>
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs
+        inline-label
+        class="bg-primary text-white shadow-2"
+      >
+        <q-route-tab
+        v-for="nav in navs"
+        :key="nav.label"
+        :to="nav.to"
+        :icon="nav.icon"
+        :label="nav.label" />
+
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :breakpoint="767"
+      :width="230"
+      show-if-above
+      bordered
+      content-class="bg-primary"
+    >
+      <q-list dark>
+        <q-item-label header>Navigation</q-item-label>
+          <q-item
+          v-for="nav in navs"
+          :to="nav.to"
+          :key="nav.label"
+          class="text-grey-4"
+          exact
+          clickable>
+            <q-item-section avatar>
+              <q-icon :name="nav.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{nav.label}}</q-item-label>
+            </q-item-section>
+          </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      navs:[
+      {
+        label:'Todo',
+        icon:'view_list',
+        to:'/'
+      },
+      {
+        label:'Settings',
+        icon:'settings',
+        to:'/settings'
+      },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @media screen and (min-width: 768px) {
+    .q-footer{
+      display: none;
+    }
+  }
+
+  .q-drawer {
+    .q-router-link--exact-active{
+      color: white !important;
+      background-color: #1e5786;
+    }
+  }
+</style>
---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-page class="q-pa-md">
+		<q-list
+			separator
+			bordered>
+
+		  <q-item
+		  	v-for="task in tasks"
+		  	:key="task.id"
+		  	@click="task.completed = !task.completed"
+		  	:class="!task.completed ? 'bg-amber-1' : 'bg-light-green-1'"
+		  	clickable
+		  	v-ripple>
+		    <q-item-section side top>
+		      <q-checkbox v-model="task.completed" />
+		    </q-item-section>
+
+		    <q-item-section>
+		      <q-item-label
+		      	:class="{ 'text-strikethrough' : task.completed }">
+		      	{{ task.name }}
+		      </q-item-label>
+		    </q-item-section>
+
+		    <q-item-section side>
+		    	<div class="row">
+		    		<div class="column justify-center">
+				    	<q-icon
+				    		name="event"
+                color="blue-13"
+				    		size="20px"
+				    		class="q-mr-xs" />
+		    		</div>
+			    	<div class="column">
+		          <q-item-label
+		          	class="row justify-end"
+		          	caption>
+		          	{{ task.dueDate }}
+		          </q-item-label>
+		          <q-item-label
+		          	class="row justify-end"
+		          	caption>
+		          	<small>{{ task.dueTime }}</small>
+		          </q-item-label>
+			    	</div>
+		    	</div>
+        </q-item-section>
+		  </q-item>
+
+		</q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      tasks:[
+        {
+          id:1,
+          name:'Go to Gym',
+          completed: false,
+          dueDate:'2020/04/27',
+          dueTime:'07:00'
+        },
+        {
+          id:2,
+          name:'Be rich',
+          completed: false,
+          dueDate:'2020/12/30',
+          dueTime:'00:00'
+        },
+        {
+          id:3,
+          name:'Go to Canada',
+          completed: false,
+          dueDate:'2022/01/01',
+          dueTime:'18:30'
+        },
+      ]
+    }
+  },
+}
+</script>
